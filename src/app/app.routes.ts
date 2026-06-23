90492838bbf43d256f3ec8a1fffda1e3a734ea24
mabrouk1 <mahmoud.mabrouk@nurishpro.com>
2026-06-23 22:26:47 +0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { UrlsNames } from './shared/models/urlsNames';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        redirectTo: UrlsNames.COURSES,
+        pathMatch: 'full',
+      },
+      {
+        path: UrlsNames.COURSES,
+        loadChildren: () => import('./core/courses/pages/courses.routes'),
+      },
+    ],
+  },
+];
